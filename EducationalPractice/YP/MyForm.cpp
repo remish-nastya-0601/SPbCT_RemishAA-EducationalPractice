9a881426e47f0ea295fb6e3503441d4e7c28fa91
remish-nastya-0601 <91561043+remish-nastya-0601@users.noreply.github.com>
2021-12-22 16:41:52 +0300

    Delete EducationalPractice/YP directory

--- EducationalPractice/YP/MyForm.cpp
+++
@@ -1,9 +0,0 @@
-#include "MyForm.h"
-#include <Windows.h>
-using namespace YP; /* вместо Project2 написать название своего проекта */
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-	Application::EnableVisualStyles();
-	Application::SetCompatibleTextRenderingDefault(false);
-	Application::Run(gcnew MyForm);
-	return 0;
-}
